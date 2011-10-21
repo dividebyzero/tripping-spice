a8f3d99b4bcfbcf503d0c033f7ae1f2eb0ef02b0
Ronan 'Zero' Schwarz <rsc@tic-mobile.de>
2011-10-21 18:16:34 +0200

    initial project commit

---
+++ src/cc/dividebyzero/swearspear/gui/SwearSpearActivity.java
@@ -0,0 +1,21 @@
+package cc.dividebyzero.swearspear.gui;
+
+import cc.dividebyzero.swearspear.R;
+import cc.dividebyzero.swearspear.R.layout;
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+public class SwearSpearActivity extends FragmentActivity {
+    
+    
+    
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+    
+    
+    
+}
